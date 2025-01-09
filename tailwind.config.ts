9d3aad50adc9499c0af853dc21a4ed758bd4117b
IgorRusskikh <russkikh200@mail.ru>
2025-01-09 22:09:44 +0700

    feat: configured tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: [
@@ -8,11 +9,28 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "tt-norms": ["var(--font-tt-norms)"],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "light-gray": "#FBFBFB",
+        "primary-red": "#D02E27",
+        black: "#1D1D1B",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          margin: "0 auto",
+          "@media (max-width: 768px)": {
+            "max-width": "91.67vw",
+          },
+        },
+      });
+    }),
+  ],
 } satisfies Config;
