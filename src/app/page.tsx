258c6feac83bc38bb508fc54002bbad35e3b8179
IgorRusskikh <russkikh200@mail.ru>
2025-01-17 18:17:34 +0700

    feat: add webapp init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import ExchangeForm from "@/widgets/ExchangeForm/ExchangeForm";
 import Header from "@/shared/Header/Header";
 import Links from "@/widgets/Links";
 import WebApp from "@twa-dev/sdk";
+import { init } from "@telegram-apps/sdk";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
+    init();
     if (typeof window !== "undefined" && window.Telegram) {
       WebApp.ready();
       console.log(WebApp);
